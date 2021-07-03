2c5580ceabcd1c39d7df820eabf9cc0d1176a518
Owen Boreham <bobrossrtx@gmail.com>
2021-07-03 23:04:19 +0100

    initial commit

---
+++ SimpleExample/SimpleExample.cpp
@@ -0,0 +1,87 @@
+#include <chrono>
+#include <iostream>
+
+// ASIO
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+// Compiler Config
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+#define ASIO_STANDALONE
+
+std::vector<char> vBuffer(1 * 1024);
+
+void GrabSomeData(asio::ip::tcp::socket& socket)
+{
+	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+		[&](std::error_code ec, std::size_t length)
+		{
+			if (!ec)
+			{
+				std::cout << "\n\nRead " << length << " bytes\n\n";
+
+				for (int i = 0; i < length; i++)
+					std::cout << vBuffer[i];
+
+				GrabSomeData(socket);
+			}
+		}
+	);
+
+}
+
+int main()
+{
+	asio::error_code ec;
+
+	// Create a "context" - essentially the platform specific interface
+	asio::io_context context;
+
+	// Give some fake tasks to asio so the context doesn't finish
+	asio::io_context::work idleWork(context);
+
+	// Start the context
+	std::thread thrContext = std::thread([&]() { context.run(); });
+
+	// Get the address of somewhere we wish to connect to
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("51.38.81.49", ec), 80);
+
+	// Create a socket, the context will deliver the implementation
+	asio::ip::tcp::socket socket(context);
+
+	// Tell socket to try and connect
+	socket.connect(endpoint, ec);
+
+	if (!ec)
+	{
+		std::cout << "Connected!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to connect to the address:\n" << ec.message() << std::endl;
+	}
+
+	if (socket.is_open())
+	{
+		
+		GrabSomeData(socket);
+
+		std::string sRequest =
+			"GET /index.html HTTP/1.1\r\n"
+			"Host: example.com\r\n"
+			"Connection: close\r\n\r\n";
+
+		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+		// Program does something else, while asio handles data transfer in background
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(2000ms);
+		
+	}
+
+	system("pause");
+	return 0;
+}
